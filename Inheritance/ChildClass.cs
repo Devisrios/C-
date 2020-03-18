8bc7ea9e7fce69db93aed8258a5e3240c14af23b
Devisrios <devithishitha98@gmail.com>
2020-03-17 18:48:59 -0600

    Add project files.

---
+++ Inheritance/ChildClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+   public class ChildClass:ParentClass
+    {
+        public void Rename(String rename)
+        {
+            name = rename;
+        }
+        static void Main(string[] args)
+        {
+           ChildClass p = new ChildClass();
+            p.SetParentClass(10, "Shree");
+            p.Rename("Devi");
+            Console.WriteLine(p.GetParentClass());
+
+        }
+    }
+}
---
+++ Inheritance/ParentClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+   public class ParentClass
+    {
+        protected int num;
+        protected String name;
+        public void SetParentClass(int n,String str)
+        {
+            num = n;
+            name = str;
+        }
+        public String GetParentClass()
+        {
+            return (name + " " + num);
+        }
+    }
+}
